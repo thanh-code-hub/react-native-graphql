8783737908484c2332d5433879bd2db82c55e8d0
Thanh Bui <thanh.v.bui511@gmail.com>
2025-10-14 10:55:12 +0300

    fix fontawesome compatibility issue

--- constants/icons.ts
+++ constants/icons.ts
@@ -5,12 +5,13 @@ import {
     faFileImport,
     faFolder
 } from '@fortawesome/free-solid-svg-icons'
-import { IconDefinition } from '@fortawesome/fontawesome-svg-core'
+import { IconDefinition } from '@fortawesome/fontawesome-common-types'
 
+// prettier-ignore
 export const icons: Record<string, IconDefinition> = {
-    FolderNode: faFolder,
-    VideoChannelNode: faCamera,
-    DigitalInputNode: faFileImport,
-    DigitalOutputNode: faFileExport,
-    AlarmNode: faBell
+    'FolderNode': faFolder,
+    'VideoChannelNode': faCamera,
+    'DigitalInputNode': faFileImport,
+    'DigitalOutputNode': faFileExport,
+    'AlarmNode': faBell
 }
